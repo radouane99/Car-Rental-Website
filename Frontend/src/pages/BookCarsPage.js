df0bdde5dbb4ff4150de76bb7565d080d508b66e
Radouane EL-asri <radouane.asri99@gmail.com>
2024-06-20 20:00:34 +0100

    Update BooCarsPages

--- Frontend/src/pages/BookCarsPage.js
+++ Frontend/src/pages/BookCarsPage.js
@@ -1,6 +1,8 @@
 import { Box, GridItem, SimpleGrid, VStack } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import path from "path";
+import dotenv from "dotenv";
 import Navbar from "../components/navbar/Navbar";
 import CarCard from "../components/ui/car-card";
 import Footer from "../components/footer";
@@ -10,20 +12,27 @@ import AvatarMenu from "../components/navbar/avatar-menu";
 import HomeSidebarContent from "../components/home/home-sidebar-content";
 import NavbarLinks from "../components/navbar/NavbarLinks";
 import SearchContext from "../SearchContext";
-// const dotenv = require("dotenv");
-// dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 function BookCars() {
-  const { searchResults, setSearchResults } = useContext(SearchContext);
-  const [cars, setCars] = useState();
+  const { searchResults } = useContext(SearchContext);
+  const [cars, setCars] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(process.env.BACKEND_URL+"/api/cars").then((response) => {
-      setCars(response.data.data);
-      setLoading(false);
-    });
+    const fetchCars = async () => {
+      try {
+        const response = await axios.get(`${process.env.BACKEND_URL}/api/cars`);
+        setCars(response.data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+        setLoading(false); 
+      }
+    };
+
+    fetchCars();
   }, []);
 
   if (isLoading) return <LoadingSpinner />;
@@ -43,23 +52,12 @@ function BookCars() {
         />
 
         <VStack>
-          <SimpleGrid
-            columns={[1, 1, 2, 2, 3]}
-            rowGap={6}
-            columnGap={8}
-            py={10}
-          >
-            {searchResults && searchResults.length > 0
-              ? searchResults.map((car) => (
-                  <GridItem key={car.id} colSpan={1}>
-                    <CarCard props={car} />
-                  </GridItem>
-                ))
-              : cars.map((car) => (
-                  <GridItem key={car.id} colSpan={1}>
-                    <CarCard props={car} />
-                  </GridItem>
-                ))}
+          <SimpleGrid columns={[1, 1, 2, 2, 3]} rowGap={6} columnGap={8} py={10}>
+            {(searchResults && searchResults.length > 0 ? searchResults : cars).map((car) => (
+              <GridItem key={car.id} colSpan={1}>
+                <CarCard props={car} />
+              </GridItem>
+            ))}
           </SimpleGrid>
         </VStack>
       </Box>
