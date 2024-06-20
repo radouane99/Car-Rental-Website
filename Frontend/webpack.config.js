7a28e4ed3ca4bf182140652ee9dba775f8a5e3a9
Radouane EL-asri <radouane.asri99@gmail.com>
2024-06-20 21:21:09 +0100

    update

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,1 +1,14 @@
-config.resolve.alias['crypto'] = 'crypto-js';
+const path = require('path');
+
+module.exports = {
+  // Your existing webpack configuration goes here
+
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+      // Other fallbacks if necessary
+    }
+  },
+
+  // Other webpack configurations as needed
+};
