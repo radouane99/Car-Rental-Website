d5324448b141ceb7952fb82749004b4cf2f9b7a0
Radouane EL-asri <radouane.asri99@gmail.com>
2024-06-20 14:41:44 +0100

    Update backend URL

--- Backend/config/app.php
+++ Backend/config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://172.19.156.216'),
 
     'asset_url' => env('ASSET_URL'),
 
--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'http://172.19.156.216:3000'],
 
     'allowed_origins_patterns' => [],
 
